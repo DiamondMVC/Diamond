781e5702d8d39230719e0498f49fa83113211bef
Jacob Jensen <jj_1337@live.dk>
2017-10-25 10:29:44 +0200

    Added additional module info
    Fogot to add additional module info to the route exception.

--- errors/exceptions/routeexception.d
+++ errors/exceptions/routeexception.d
@@ -1,3 +1,8 @@
+/**
+* Copyright © DiamondMVC 2016-2017
+* License: MIT (https://github.com/DiamondMVC/Diamond/blob/master/LICENSE)
+* Author: Jacob Jensen (bausshf)
+*/
 module diamond.errors.exceptions.routeexception;
 
 import diamond.core.apptype;
