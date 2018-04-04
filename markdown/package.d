a1a753b375304ae50b46498be0c332af7c3f56d8
Jacob Jensen <jj_1337@live.dk>
2018-04-04 23:29:27 +0200

    markdown package

---
+++ markdown/package.d
@@ -0,0 +1,13 @@
+/**
+* Copyright © DiamondMVC 2018
+* License: MIT (https://github.com/DiamondMVC/Diamond/blob/master/LICENSE)
+* Author: Jacob Jensen (bausshf)
+*/
+module diamond.markdown;
+
+public
+{
+  import diamond.markdown.parser;
+  import diamond.markdown.part;
+  import diamond.markdown.type;
+}
