37394bd53e73452fdc69a9d66527acd13946263f
Jacob Jensen <jj_1337@live.dk>
2018-06-13 23:39:52 +0200

    Stand-alone is no longer supported.

--- init/diamondapp.d
+++ init/diamondapp.d
@@ -7,26 +7,28 @@ module diamondapp;
 
 import diamond.core : isWeb;
 
-static if (isWeb)
-{
-  public import diamond.init.web;
-}
-else
-{
-  import diamond.views;
+public import diamond.init.web;
 
-  mixin GenerateViews;
-
-  static foreach (viewResult; generateViewsResult)
-  {
-    mixin("#line 1 \"view: " ~ viewResult.name ~ "\"\n" ~ viewResult.source);
-  }
-
-  mixin GenerateGetView;
-
-  /// Shared static constructor for stand-alone applications.
-  shared static this()
-  {
-    // ...
-  }
-}
+// static if (isWeb)
+// {
+//
+// }
+// else
+// {
+//   import diamond.views;
+//
+//   mixin GenerateViews;
+//
+//   static foreach (viewResult; generateViewsResult)
+//   {
+//     mixin("#line 1 \"view: " ~ viewResult.name ~ "\"\n" ~ viewResult.source);
+//   }
+//
+//   mixin GenerateGetView;
+//
+//   /// Shared static constructor for stand-alone applications.
+//   shared static this()
+//   {
+//     // ...
+//   }
+// }
